d6de009d3e832883ff78ab06caeb7076ddf678cc
cspgithub <cspandey3000@gmail.com>
2021-04-02 12:54:32 +0530

    rt

--- BDDE2E/test-output-thread/report.js
+++ BDDE2E/test-output-thread/report.js
@@ -4,9 +4,9 @@ CucumberHTML.timelineItems.pushArray([
     "id": "automated-end2end-tests;verify-login-page-successfully-loaded",
     "feature": "Automated End2End Tests",
     "scenario": "Verify Login Page successfully Loaded",
-    "start": 64660879,
-    "end": 64699437,
-    "group": 12,
+    "start": 98319738,
+    "end": 98359748,
+    "group": 14,
     "content": "",
     "className": "passed",
     "tags": ""
@@ -15,9 +15,9 @@ CucumberHTML.timelineItems.pushArray([
     "id": "automated-end2end-tests;verify22-login-page-successfully-loaded",
     "feature": "Automated End2End Tests",
     "scenario": "Verify22 Login Page successfully Loaded",
-    "start": 64660879,
-    "end": 64698407,
-    "group": 13,
+    "start": 98319738,
+    "end": 98359732,
+    "group": 15,
     "content": "",
     "className": "passed",
     "tags": ""
@@ -25,11 +25,11 @@ CucumberHTML.timelineItems.pushArray([
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 12,
+    "id": 14,
     "content": "Thread[pool-1-thread-1,5,main]"
   },
   {
-    "id": 13,
+    "id": 15,
     "content": "Thread[pool-1-thread-2,5,main]"
   }
 ]);
